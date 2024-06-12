70b46f23405177a850d0621c08b551e8c5e84c2e
L0laapk3 <L0laapk3@gmail.com>
2024-06-12 15:41:22 +0200

    improve setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,26 @@
 #!/bin/zsh
 
+dstDir=${1:-$HOME}
+srcDir=${2:-$(dirname $0)}
+
 # Make links for all files to dotfiles folder
-for file in $(realpath $(dirname $0))/**/.*; do # Everything starting with dot
-	if [[ -d $file ]]; then # Skip directories
+for file in "$srcDir"/**/.*; do # Everything starting with dot
+	if [[ -d "$file" ]]; then # Skip directories
 		continue
 	fi
-	echo                                          $(realpath --relative-to=$(realpath $(dirname $0)) $file)
-	mkdir -p                          ~/$(dirname $(realpath --relative-to=$(realpath $(dirname $0)) $file))
-	ln -s $(realpath --relative-to=$HOME $file) ~/$(realpath --relative-to=$(realpath $(dirname $0)) $file)
+	src=$(realpath --relative-to="$dstDir" "$file")
+	dst=$(realpath --relative-to="$srcDir" "$file")
+	mkdir -p $(dirname ~/"$dst")
+	result=""
+	if [[ -L ~/"$dst" ]]; then
+		rm ~/"$dst"
+		result="(replaced)"
+	fi
+	if [[ -e ~/"$dst" ]]; then
+		result="(exists)"
+	else
+		ln -s ~/"$src" ~/"$dst"
+	fi
+
+	printf "~/%-19s -> ~/%-28s %s\n" "$dst" "$src" $result
 done
