a67fc66d99cd3ee3778e1e8f06ae395a0c5c17de
L0laapk3 <L0laapk3@gmail.com>
2024-06-19 18:02:12 +0200

    download font script

---
+++ fonts.sh
@@ -0,0 +1,2 @@
+#! /bin/env zsh
+wget -nc -P ~/.fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
